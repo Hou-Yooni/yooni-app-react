b6dbad2549741e3ede6e9cac1163d296e536c040
yooni hou <yooni.hou@globeunion.com>
2022-06-30 17:42:18 +0800

    feat: headerBtn css

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cartContext'
 const HeaderCartButton = (props) => {
+  const [btnIsHeighted, setBtnIsHeighted] = useState(false)
   const cartCtx = useContext(CartContext)
   const numberOfCartItems = cartCtx.items.reduce((acc, curr) => {
     return acc + curr.amount
   }, 0)
+
+  const btnClasses = `${classes.button} ${btnIsHeighted ? classes.bump : ''}`
+
+  const { items } = cartCtx
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnIsHeighted(true)
+    const timer = setTimeout(() => {
+      console.log('inner')
+      setBtnIsHeighted(false)
+    }, 300)
+
+    return () => {
+      console.log('return')
+      clearTimeout(timer)
+    }
+  }, [items])
   return (
     <>
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
