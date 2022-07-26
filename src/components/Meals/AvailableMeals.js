18ae53e8bdabe86f251f79d216da327b4b0a584b
Yooni Hou <mooiao@yahoo.com.tw>
2022-07-27 01:25:14 +0800

    feat: final create

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import Card from '../UI/Card'
 import MealItem from './MealItem/MealItem'
 import classes from './AvailableMeals.module.css'
@@ -28,7 +29,57 @@ const DUMMY_MEALS = [
   }
 ]
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState()
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-dessert-default-rtdb.firebaseio.com/meals.json')
+      const responseData = await response.json()
+
+      const loadedMeals = []
+      for (const key in responseData) {
+        loadedMeals.push({
+          ...responseData[key],
+          id: key
+        })
+      }
+      setMeals(loadedMeals)
+      setIsLoading(false)
+    }
+    fetchMeals().catch((error) => {
+      setIsLoading(false)
+      setHttpError(error.message)
+    })
+
+    //fetchMeals是一個異步函數，因此他總是返回一個response，但是如果現在拋出一個錯誤導致response不能被處理，那麼就會被拋出來 並不會執行下方try catch
+    // try {
+    //   fetchMeals()
+    // } catch (error) {
+    //   setIsLoading(false)
+    //   setHttpError(error.message)
+    // }
+  }, [])
+
+  //寫在這邊的原因是因為想要isLoading = false時 就不會觸發整個組件的更新，
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem description={meal.description} key={meal.id} id={meal.id} name={meal.name} price={meal.price} />
   ))
 
